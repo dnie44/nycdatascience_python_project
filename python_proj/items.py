b32aa94479f7cfe3ffd223ca3ffc3dc3de4d7e73
Daniel Nie <danielx.nie@gmail.com>
2021-10-04 13:31:36 -0400

    first commit: started new scrapy project and generated spider

---
+++ python_proj/items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PythonProjItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    prod_sku = scrapy.Field()
+    prod_name= scrapy.Field()
